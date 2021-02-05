5ea20aa129cbf81ba659cf26cbc1199b1198bcae
Vekisis <damianburbo@yahoo.com>
2021-02-05 11:50:01 -0500

    Added Constructor

--- index.js
+++ index.js
@@ -16,79 +16,79 @@ start();
 async function start() {
   const welcomeMessage = `You awake with a sharp pain in the front of your head. Still slightly inebriated from the New Years party last night. \nYou look around to find yourself in a dungeon that is not your own. \nIn the middle of the room you see a piece of paper and pen labeled CONTACT TRACING, please fill out your name to continue: `;
   let answer = await ask(welcomeMessage);
-  console.log( `You write ${answer} at the bottom of the page, barely legible. ` + "\nLooks like 2020 isn't over yet, YOU MUST ESCAPE! You look around and see a single door in the room labelled SOUTH, how convenient. \nPlease write out your actions in the [ACTIONS] [DIRECTIONS] format. Type help for a word list. " );
-
+  command = await ask( `You write ${answer} at the bottom of the page, barely legible. ` + "\nLooks like 2020 isn't over yet, YOU MUST ESCAPE! You look around and see a single door in the room labelled SOUTH, how convenient. \nPlease write out your actions in the [ACTIONS] [DIRECTIONS] format. Type help for a word list. " );
+  if(command === "help" || command === '/help' || command === "h"){
+    console.log(`Here are your commands: ${commands}`)
+    }
   // console.log("Now write your code to make this work!"");
   // process.exit();
 }
 let currentRoom = "start";
 let commands = [
-  "pick up",
-  "inventory",
-  "use",
-  "look",
-  "look around",
-  "open",
-  "attack",
-  "examine",
-  "inspect",
-  "exit",
-  "go",
-  "talk",
-  "run",
-  "catch",
+  " pick up",
+  " inventory (i)",
+  " use",
+  " open",
+  " attack",
+  " inspect",
+  " exit",
+  " go (g)",
+  " talk",
+  " run",
+  " catch",
+  " help (h)",
 ];
   
-let rooms = {
-  "start": {
-      "description": "This is the room you woke up in! it is dark and smells like mildew. To advance you must go south" ,
-      "directions" :{
-          "south": south,
-          "west": west
+// let rooms = {
+//   "start": {
+//       "description": "This is the room you woke up in! it is dark and smells like mildew. To advance you must go south" ,
+//       "directions" :{
+//           "south": south,
+//           "west": west
           
-      }
-  },
-  "Hallway": {
-      "description": "You enter a long dark hallway. All though the hallway is dark you are able to make out 3 doors. One to the south, east and west. \n Which door do you decide to travel through?",
-      "directions": {
-          "north": "start",
-          "south": "room name",
-          "east": "room name",
-          "west" : 
-      }
-  },
-  "room title status": {
-      "description": "room description",
-      "directions": {
-          "south": "room name"
-      }
-  },
-  "room title status": {
-      "description": "room description",
-      "directions": {
-          "west": "room name"
-      }
-  },
-  "room title status": {
-      "description": "room description",
-      "directions": {
-          "east": "room name",
-          "west": "room name"
-      }
-  },
-  "room title status": {
-      "description": "room description",
-      "directions": {
-          "east": "room name"
-      }
-  }
-  "room title status": {
-      "description": "room description",
-      "directions": {
-          "east": "room name"
-      }
-  }
-}
+//       }
+//   },
+//   "Hallway": {
+//       "description": "You enter a long dark hallway. All though the hallway is dark you are able to make out 3 doors. One to the south, east and west. \n Which door do you decide to travel through?",
+//       "directions": {
+//           "north": "start",
+//           "south": "room name",
+//           "east": "room name",
+//           "west" : 
+//       }
+//   },
+//   "room title status": {
+//       "description": "room description",
+//       "directions": {
+//           "south": "room name"
+//       }
+//   },
+//   "room title status": {
+//       "description": "room description",
+//       "directions": {
+//           "west": "room name"
+//       }
+//   },
+//   "room title status": {
+//       "description": "room description",
+//       "directions": {
+//           "east": "room name",
+//           "west": "room name"
+//       }
+//   },
+//   "room title status": {
+//       "description": "room description",
+//       "directions": {
+//           "east": "room name"
+//       }
+//   }
+//   "room title status": {
+//       "description": "room description",
+//       "directions": {
+//           "east": "room name"
+//       }
+//   }
+// }
 let directions = {
   north: ["south", "east", "west", "north"],
   east: ["south", "east", "west", "north"],
@@ -98,7 +98,7 @@ let directions = {
 let inventory = ["key", "calendar", "torch", "rope", "tp"];
 //Changing Room Function
 function changeRoom(nextRoom) {
-  if (directions[currentRoom].includes(nextRoom)) {
+  if (rooms[currentRoom].includes(nextRoom)) {
     currentRoom = nextRoom;
   } else {
     Console.log(
---
+++ roomconstructor.js
@@ -0,0 +1,66 @@
+class Rooms{
+    constructor(room, direction, description){
+        this.room = room;
+        this.direction = direction;
+        this.description = description;
+    }
+    changeTo(nextRoom){
+        if(this.direction.includes(nextRoom)){
+            this.room = nextRoom
+            console.log(this.description)
+        }  else {
+            Console.log(`BONK!! You hit a wall`);
+    }
+}
+
+
+// let rooms = {
+//   "start": {
+//       "description": "This is the room you woke up in! it is dark and smells like mildew. To advance you must go south" ,
+//       "directions" :{
+//           "south": south,
+//           "west": west
+          
+//       }
+//   },
+//   "Hallway": {
+//       "description": "You enter a long dark hallway. All though the hallway is dark you are able to make out 3 doors. One to the south, east and west. \n Which door do you decide to travel through?",
+//       "directions": {
+//           "north": "start",
+//           "south": "room name",
+//           "east": "room name",
+//           "west" : 
+//       }
+//   },
+//   "room title status": {
+//       "description": "room description",
+//       "directions": {
+//           "south": "room name"
+//       }
+//   },
+//   "room title status": {
+//       "description": "room description",
+//       "directions": {
+//           "west": "room name"
+//       }
+//   },
+//   "room title status": {
+//       "description": "room description",
+//       "directions": {
+//           "east": "room name",
+//           "west": "room name"
+//       }
+//   },
+//   "room title status": {
+//       "description": "room description",
+//       "directions": {
+//           "east": "room name"
+//       }
+//   }
+//   "room title status": {
+//       "description": "room description",
+//       "directions": {
+//           "east": "room name"
+//       }
+//   }
+// }
