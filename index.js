4eee357ff3089db4ff696798b401b9919abce3ad
Austen Sulikowski <Austenski@gmail.com>
2021-02-05 10:55:44 -0500

    zork

--- index.js
+++ index.js
@@ -38,6 +38,57 @@ let commands = [
   "run",
   "catch",
 ];
+  
+let rooms = {
+  "start": {
+      "description": "This is the room you woke up in! it is dark and smells like mildew. To advance you must go south" ,
+      "directions" :{
+          "south": south,
+          "west": west
+          
+      }
+  },
+  "Hallway": {
+      "description": "You enter a long dark hallway. All though the hallway is dark you are able to make out 3 doors. One to the south, east and west. \n Which door do you decide to travel through?",
+      "directions": {
+          "north": "start",
+          "south": "room name",
+          "east": "room name",
+          "west" : 
+      }
+  },
+  "room title status": {
+      "description": "room description",
+      "directions": {
+          "south": "room name"
+      }
+  },
+  "room title status": {
+      "description": "room description",
+      "directions": {
+          "west": "room name"
+      }
+  },
+  "room title status": {
+      "description": "room description",
+      "directions": {
+          "east": "room name",
+          "west": "room name"
+      }
+  },
+  "room title status": {
+      "description": "room description",
+      "directions": {
+          "east": "room name"
+      }
+  }
+  "room title status": {
+      "description": "room description",
+      "directions": {
+          "east": "room name"
+      }
+  }
+}
 let directions = {
   north: ["south", "east", "west", "north"],
   east: ["south", "east", "west", "north"],
