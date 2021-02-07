ef00b3392e6350c18565574267597cbb6a014d18
Vekisis <damianburbo@yahoo.com>
2021-02-07 18:15:53 -0500

    Completed functional game. Built working constructor.

--- index.js
+++ index.js
@@ -14,151 +14,107 @@ function ask(questionText) {
 start();
 
 async function start() {
-  
-  const welcomeMessage = `You awake with a sharp pain in the front of your head. Still slightly inebriated from the New Years party last night. \nYou look around to find yourself in a dungeon that is not your own. \nIn the middle of the room you see a piece of paper and pen labeled CONTACT TRACING, please fill out your name to continue: `;
+  const welcomeMessage = `You awake with a sharp pain in the front of your head. Still slightly inebriated from the New Years party last night. \nYou look around to find yourself in a dungeon that is not your own. Figure it's about time to head out. \nIn the middle of the room you see a piece of paper and pen labelled CONTRACT TRACING. Please fill out your name to continue: `;
   let name = await ask(welcomeMessage);
-  answer = await ask( `You write ${name} at the bottom of the page, barely legible. ` + "\nLooks like 2020 isn't over yet, YOU MUST ESCAPE! , how convenient. \nPlease write out your actions in the [ACTIONS] [DIRECTIONS] format. Type help for a word list. " );
-  if(answer === "help" || answer === '/help' || answer === "h"){
-    console.log(`Here are your commands: ${commands}`)
-    }
-    beginReply = await ask(`${name}, are you ready to begin your escape? `)
-    if( beginReply ===  "yes"|| beginReply === "y"){
-    console.log( " You look around and see a single door in the room labelled SOUTH")
-    } else if(beginReply !== ["no" , "n"]) {
-    console.log(" You decide you're better off here and go to sleep!")
-    process.exit()
-    } else 
-  console.log("That's not an answer!")
-  let rooms = new
+  console.log(
+    `You write ${name} at the bottom of the page, barely legible. \nLooks like 2020 isn't over yet. `
+  );
+
+  //Asks you to begin the quest. Details are below
+  beginReply = await ask(`${name}, are you ready to begin your escape? `);
   
-  class Rooms{
-    constructor(room, direction, description){
-        this.room = room;
-        this.direction = direction;
-        this.description = description;
+  
+// Class constructor for the rooms.
+  class Rooms {
+    constructor(room, description, direction,) {
+      this.room = room;
+      this.direction = direction;
+      this.description = description;
+    }
+    //Function to move between rooms
+    changeTo(direction) {
+      if (this.direction.includes(direction)) {
+        currentRoom = lookUpTable[direction];
+      } else {
+        console.log(`BONK!! You hit a wall. Or you didn't go into the case sensitive door.`);
+      }
     }
   }
-  dir = await ask("What direction do you go?")
-  // if(dir === "south" || dir === "s" || dir === "go south"){
-    function changeTo(nextRoom)
-      if(directions[rooms].includes(nextRoom)){
-          this.room = nextRoom
-          console.log(this.description)
-      }  else {
-          console.log(`BONK!! You hit a wall`);
-  }
-
-
-}
-
-
-let currentRoom = "start";
-let commands = [
-  " pick up",
-  " inventory (i)",
-  " use",
-  " open",
-  " attack",
-  " inspect",
-  " exit",
-  " go (g)",
-  " talk",
-  " run",
-  " catch",
-  " help (h)",
-  "north",
-  "east",
-  "south",
-  "west"
-];
+  //Section that describes each room in the class. The "room" is the overall name of the room and the identifier needed to traverse between them. The description tells you what and how to move about each room, and the direction is used to go between them.
+  let hallRoom = new Rooms(
+    "Hall Room",
+    "You enter a large square hall. Although the room is dark you are able to make out three doors. One labelled closet, one labelled safe room, and a room down another long hall. The number '2' is written on a wall pointing towards the closet door. \nWhich door do you decide to travel through? \n[Start] [Broom Closet] [Safe Room] [Long Room]",
+    ["Start", "Broom Closet", "Safe Room", "Long Room",]
+  );
+  //Starting room. Only one exit.
+  let startRoom = new Rooms(
+    "Start",
+    "This is the room you woke up in! it is dark and smells like mildew. The number '1' is labelled on the wall. This may be useful later. You can go to \n[Hall Room]",
+    ["Hall Room"],
+  );
+  //Auxillary room. Has a clue to the puzzle like every other room
+  let broomCloset = new Rooms(
+      "Broom Closet",
+      "You enter a small broom closet. Instead of a broom or anything for that matter, the number '3' in huge font on the floor. Wow, isn't this just an obvious puzzle staring you in the face. The only way out is back the way you came. \n[Hall Room]",
+      ["Hall Room"]
+  );
+  //Room with a safe. oooooooo
+  let safeRoom = new Rooms(
+      "Safe Room",
+      "You enter a dimly lit room. An open safe is visible in the middle of the room labelled with the number '4'. The only way out is back the way you came. \n[Hall Room]",
+      ["Hall Room"],
+  )
+  //Long room needed to get to the end
+  let longRoom = new Rooms(
+      "Long Room",
+      "You venture further down a empty hallway for fifteen miles, give or take a few. Halfway down you see the number '5' painted on the ceiling. \nDespite the distance travelled, you can just type [Hall Room] or [Final Room] to go to the next room instantly. Must be nice.",
+      ["Hall Room", "Final Room"],
+  )
+  //Room that requires solving the puzzle which is just typing 12345 into the terminal
+  let finalRoom = new Rooms(
+      "Final Room",
+      "After the long and arduous journey you near the exit. However you must type in the five digit password at the end of the door. No cheating, unless you watched Spaceballs and know the password already. Otherwise, return to the [Long Room]",
+      ["12345", "Long Room"]
+  )
+  //Room that exists to tell you that you won!
+  let endRoom = new Rooms(
+      "12345",
+      "At last your really short journey has come to an end. You've now entered 2021 much like you left 2020, hopeless and grim. I know it may seem daunting, but good luck! You can't leave. Unless you want to go back to the dungeon, weirdo.",
+      ["Final Room"]
+  )
   
-destinations = {
-  //starting room
-  "start": {
-      "description": "This is the room you woke up in! it is dark and smells like mildew. To advance you must go south" ,
-      "directions" :{
-          "south": "south"
-          
-          
-      }
-  },
-  //south1
-  "Hall Room": {
-      "description": "You enter a long dark hallway. Although the hallway is dark you are able to make out 3 doors. One to the south, east, west as well as the door behind you. \n Which door do you decide to travel through?",
-      "directions": {
-          "north": "start",
-          "south": "south2",
-          "east": "east0",
-          "west" : "west0"
-      }
-  },
-  //east0
-  "Key Room": {
-      "description": "You enter a small broom closet. Instead of a broom, there's a key glistening on the floor. Wow, isn't this just a convenient plot device. The only way out is back the way you came.",
-      "directions": {
-          "west": "south1"
-      }
-  },
-  //west0
-  "Chest Room": {                                        
-      "description": "You enter a dimly lit room. A chest is visible in the middle of the room labelled 'Do not open until 2021' on top. The only way out is back the way you came.",
-      "directions": {
-          "east": "south1"
-      }
-  },
-  //south2
-  "Empty Hallway": {
-      "description": "You venture further down a empty hallway for 15 miles, give or take a few. Luckily for you you can just type south or north to go to the next room instantly. Must be nice. ",
-      "directions": {
-          "north": "south1",
-          "south": "south3"
-      }
-  },
-  //south 3
-  "Final Room": {
-      "description": "After the long and arduous journey you near the exit. You know, I just realized we don't need a scruffy Robin Williams Jumanji reference if the puzzle is the key and the chest for the calendar, or maybe another key for a south 2 door. Just type in south and finish the game already good lord.",
-      "directions": {
-          "north": "south2",
-          " south" : "exit",
-       
-
-      }
-  },
-  //exit room
-  "exit room": {
-      "description": "At last your really short journey has come to an end. You've now entered 2021 much like you left 2020, hopeless and grim. Anyway I have to get going, good luck!",
-      "directions": {
-          "none": "none"
-      }
+  
+  //Table to show each room
+  lookUpTable = {
+      Start: startRoom,
+      'Hall Room' : hallRoom,
+      'Broom Closet': broomCloset,
+      'Safe Room': safeRoom,
+      'Long Room': longRoom,
+      'Final Room': finalRoom,
+      '12345': endRoom
   }
-}
 
-let directions = {
-  north: ["south", "east", "west", "north"],
-  east: ["south", "east", "west", "north"],
-  south: ["south", "east", "west", "north"],
-  west: ["south", "east", "west", "north"],
-};
-let inventory = ["key", "calendar", "torch", "rope", "tp"];
-//Changing Room Function
-function changeRoom(nextRoom) {
-  if (rooms[currentRoom].includes(nextRoom)) {
-    currentRoom = nextRoom;
-  } else {
-    Console.log(
-      `BONK!! You ran into a wall`
-    );
-  } // Trying to allow room transferring
-}
-//Function to show the commands
-function showCommands() {
-  if(command === "help" || command === '/help' || command === "h"){
-  console.log(`Here are your commands: ${commands}`)
+  
+//Function that starts the game up. I believe it's needed here for scope reasons. But as the saying goes, "If it ain't broke..."
+async function play() {
+    currentRoom = startRoom;
+    if (beginReply === "yes" || beginReply === "y") {
+            console.log("Great, let's go! ");
+          } else if (beginReply === "no" || beginReply === "n") {
+            console.log(
+              "You go back to sleep, where at least you're safe inside your mind. "
+            );
+            process.exit();
+          } else {
+            console.log(`What's that? "I'm so stoked?" Great, let's go!`);
+          }
+    let response = await ask("You must type your responses in CASE SENSITIVE manner. Press Enter to continue ");
+    while (response !== "no" || response !== "n") {
+      console.log(currentRoom.description);
+      response = await ask(">");
+      currentRoom.changeTo(response);
+    }
   }
-}
-
-function showInv(){
-  if(bag === 'i' || bag == 'inventory' || bag === 'inv'){
-console.log( `Inventory ${inventory}`)
-  }
+  play();
 }
